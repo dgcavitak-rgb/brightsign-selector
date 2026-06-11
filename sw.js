be805edfcf52af85470b6fc84f5af11df8fd5a9b
dgcavitak-rgb <dg.cavitak@gmail.com>
2026-06-11 16:32:16 +0530

    6546

--- sw.js
+++ sw.js
@@ -78,7 +78,7 @@
 // the activate handler. Promotes hard-refresh semantics for users with the
 // PWA installed.
 
-const CACHE_NAME = 'brightsign-v1.5.2';
+const CACHE_NAME = 'brightsign-v1.5.4';
 const SHELL_URLS = [
   './',
   './index.html'
